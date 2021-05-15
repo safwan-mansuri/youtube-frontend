b7a5b8089a990e2fb4272b91087d2d363a1d8b48
safwan <safwan.mansuri@peak.ai>
2021-05-15 17:20:36 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [as, setFile] = useState('');
+  const [data, setData] = useState(null);
+
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+  }
+
+  const show = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', as, as.name);
+    const url = 'http://127.0.0.1:5000/upload';
+
+    const resp2 = await axios.post(url, formData, {headers: {
+      'Content-Type': 'multipart/form-data',
+    }});
+
+    const datas = JSON.parse(resp2.data.body)
+    console.log(datas)
+    setData(datas);
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='file' name='file' onChange={handleChange}/>
+      <button type='submit' onClick={show}>submit</button>
+      {console.log(data)}
+      {
+        data ? (
+          <div>
+          {
+            Object.keys(data).map((ele) => {
+              return <h1><span>{ele}: </span>{data[ele]}</h1>
+            })
+          }
+          </div>
+      ) 
+        : (<h1>not there</h1>)
+      }
     </div>
   );
 }
